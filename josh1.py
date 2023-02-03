9eacc554735b7a315eb757332fb713464b314dec
jf11235 <josh.h.feinberg@gmail.com>
2023-02-02 19:51:28 -0800

    notes update to include questions

--- josh1.py
+++ josh1.py
@@ -1,7 +1,8 @@
 import csv
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
-census_csv = pd.read_csv('pulse2023_puf_53.csv')
+census_csv = pd.read_csv('/PUFs/clean_master_merge.csv')
 
 census_csv.head()
